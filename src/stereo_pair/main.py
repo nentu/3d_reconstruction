e42948e6e14ab3cdcc528e01fa36358af9b1c504
Zaytsev Artem <zajtsev.artjom@samlit.net>
2025-02-12 12:21:59 +0300

    Add projected models in stereo pait module

--- src/stereo_pair/main.py
+++ src/stereo_pair/main.py
@@ -2,29 +2,29 @@ import cv2
 import numpy as np
 
 from src.basic_graphic.models.cube import Cube
-from src.basic_graphic.models.point import Point
 from src.basic_graphic.draw_utils import draw_model, get_depth_map
-from src.basic_graphic.utils import get_intrinsic_matrix, rotate
-from src.basic_graphic.models.model import Model
+from src.basic_graphic.utils import get_intrinsic_matrix
 from src.stereo_pair.stereo_pair import StereoPair, get_rt_matrix, make_homogeneous
 
 
 def _draw_model(plane_shape, model, name):
-    model.apply_tranform(intrinsic_matrix)
-    plane = get_depth_map(plane_shape, model)
+    projected_matrix = model.apply_tranform(intrinsic_matrix)
+    plane = get_depth_map(plane_shape, projected_matrix)
     plane += 255
-    draw_model(plane, model)
+    draw_model(plane, projected_matrix)
     cv2.imshow(name, plane)
+    return projected_matrix
 
 
 if __name__ == "__main__":
-    plane_shape = np.array([300, 300])
+    win_size = 400
+    plane_shape = np.array([win_size, win_size])
     camera_angle = (90, 90)
 
-    left_camera_pos = np.array([0, 0, 0])
-    right_camera_pos = np.array([1, 0, 0])
+    left_camera_pos = np.array([-31, 40, 10])
+    right_camera_pos = np.array([10, 24, -354])
 
-    left_camera_rot = np.array([0, 0, 0])
+    left_camera_rot = np.array([20, 0, 0])
     right_camera_rot = np.array([0, 0, 0])
 
     intrinsic_matrix = get_intrinsic_matrix(*camera_angle, *plane_shape)
@@ -34,6 +34,8 @@ if __name__ == "__main__":
 
     inv_left_rt_matrix = np.linalg.inv(left_rt_matrix)
     inv_right_rt_matrix = np.linalg.inv(right_rt_matrix)
+
+    # Class instance for 3d coordinates computing
     sp = StereoPair(
         left_rt_matrix,
         right_rt_matrix,
@@ -41,34 +43,23 @@ if __name__ == "__main__":
         intrinsic_matrix,
     )
 
-    r = 99
+    r = 99  # Cube size
 
-    # print(angle_coordinates)
-
-    shift = 0
     rotation = 180
 
-    h = 0
-    k = 1
-
-
-
-
     while True:
-        model = Cube(r) # ObjModel('../graphic/models/cow.obj')
+        model = Cube(r)  # ObjModel('../graphic/models/cow.obj')
 
-        general_rt_matrix = get_rt_matrix([180, rotation, 0], [0, 0, 300 + shift])
-        model.vertex_list = general_rt_matrix.dot(
+        # Move cube
+        cube_rt_matrix = get_rt_matrix([180, rotation, 0], [0, 0, 300])
+        model.vertex_list = cube_rt_matrix.dot(
             make_homogeneous(model.vertex_list, 1).T
         ).T[:, :3]
-
-        # rotate(model, 180, rotation, 0)
-
-        # model.vertex_list[:, 2] += 300 + shift
 
         model_r = model.copy()
         model_l = model.copy()
 
+        # Move cameras
         model_l.vertex_list = inv_left_rt_matrix.dot(
             make_homogeneous(model_l.vertex_list, 1).T
         ).T[:, :3]
@@ -77,36 +68,33 @@ if __name__ == "__main__":
             make_homogeneous(model_r.vertex_list, 1).T
         ).T[:, :3]
 
-        # model_r.vertex_list -= inv_right_rt_matrix.dot(make_homogeneous(model_r.vertex_list, 1))
-
-        _draw_model(plane_shape, model_l, "camera_left")
-        _draw_model(plane_shape, model_r, "camera_right")
-
+        # Get 3d coordinates
         res = sp.compute_3d(
-            model_l.vertex_list[:, :2],  #  .astype(np.int16),
-            model_r.vertex_list[:, :2],  #  .astype(np.int16),
+            model_l.apply_tranform(intrinsic_matrix).vertex_list[
+                :, :2
+            ],  #  .astype(np.int16),
+            model_r.apply_tranform(intrinsic_matrix).vertex_list[
+                :, :2
+            ],  #  .astype(np.int16),
         )
-
         res_model = Cube(r)
         res_model.vertex_list = np.array(res)
 
+        # Compute error
         er = np.abs(model.vertex_list - res)
-        # print(res[0])
-
         if np.linalg.norm(er) > 1e-2:
             print("Failed")
             print(np.mean(er, axis=0).astype(np.int16))
             print(np.std(er, axis=0))
-        # print("==")
+
+        # Draw models
         _draw_model(plane_shape, res_model, "res")
+        _draw_model(plane_shape, model_l, "camera_left")
+        _draw_model(plane_shape, model_r, "camera_right")
 
-        # _draw_model(plane_shape, model, "camera_center", True)
-
-        if cv2.waitKey(0) == ord("q"):
+        if cv2.waitKey(1) == ord("q"):
             break
 
-        shift += 0
         rotation += 0.1
-        # k += 2e-3
 
     cv2.destroyAllWindows()
