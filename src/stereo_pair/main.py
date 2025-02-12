7757242ba17d95e4d01666caeeb7782f20ee5fb9
Zaytsev Artem <zajtsev.artjom@samlit.net>
2025-02-12 12:26:22 +0300

    Differnt instrinsic matrix for cameras and full view

--- src/stereo_pair/main.py
+++ src/stereo_pair/main.py
@@ -7,7 +7,7 @@ from src.basic_graphic.utils import get_intrinsic_matrix
 from src.stereo_pair.stereo_pair import StereoPair, get_rt_matrix, make_homogeneous
 
 
-def _draw_model(plane_shape, model, name):
+def _draw_model(plane_shape, model, name, intrinsic_matrix):
     projected_matrix = model.apply_tranform(intrinsic_matrix)
     plane = get_depth_map(plane_shape, projected_matrix)
     plane += 255
@@ -18,7 +18,9 @@ def _draw_model(plane_shape, model, name):
 
 if __name__ == "__main__":
     win_size = 400
-    plane_shape = np.array([win_size, win_size])
+    main_plane_shape = np.array([win_size, win_size])
+    camera_plane_shape = main_plane_shape // 2
+
     camera_angle = (90, 90)
 
     left_camera_pos = np.array([-31, 40, 10])
@@ -27,7 +29,8 @@ if __name__ == "__main__":
     left_camera_rot = np.array([20, 0, 0])
     right_camera_rot = np.array([0, 0, 0])
 
-    intrinsic_matrix = get_intrinsic_matrix(*camera_angle, *plane_shape)
+    main_intrinsic_matrix = get_intrinsic_matrix(*camera_angle, *main_plane_shape)
+    camera_intrinsic_matrix = get_intrinsic_matrix(*camera_angle, *camera_plane_shape)
 
     left_rt_matrix = get_rt_matrix(left_camera_rot, left_camera_pos)
     right_rt_matrix = get_rt_matrix(right_camera_rot, right_camera_pos)
@@ -39,8 +42,8 @@ if __name__ == "__main__":
     sp = StereoPair(
         left_rt_matrix,
         right_rt_matrix,
-        intrinsic_matrix,
-        intrinsic_matrix,
+        camera_intrinsic_matrix,
+        camera_intrinsic_matrix,
     )
 
     r = 99  # Cube size
@@ -70,10 +73,10 @@ if __name__ == "__main__":
 
         # Get 3d coordinates
         res = sp.compute_3d(
-            model_l.apply_tranform(intrinsic_matrix).vertex_list[
+            model_l.apply_tranform(camera_intrinsic_matrix).vertex_list[
                 :, :2
             ],  #  .astype(np.int16),
-            model_r.apply_tranform(intrinsic_matrix).vertex_list[
+            model_r.apply_tranform(camera_intrinsic_matrix).vertex_list[
                 :, :2
             ],  #  .astype(np.int16),
         )
@@ -88,9 +91,9 @@ if __name__ == "__main__":
             print(np.std(er, axis=0))
 
         # Draw models
-        _draw_model(plane_shape, res_model, "res")
-        _draw_model(plane_shape, model_l, "camera_left")
-        _draw_model(plane_shape, model_r, "camera_right")
+        _draw_model(main_plane_shape, res_model, "res", main_intrinsic_matrix)
+        _draw_model(camera_plane_shape, model_l, "camera_left", camera_intrinsic_matrix)
+        _draw_model(camera_plane_shape, model_r, "camera_right", camera_intrinsic_matrix)
 
         if cv2.waitKey(1) == ord("q"):
             break
